a2f77911a0796b924f2bd29289d448dfdbca992c
nhungnguyen-orm <nhung.nguyen@renovacloud.com>
2026-01-25 13:52:49 +0700

    setup header-footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-
+import { Toaster } from "sonner"
+import { Header } from "./components/header"
+import { Footer } from "./components/footer"
 
 function App() {
-
   return (
     <>
-      WELCOME TO HOMESTAY
+      <div className="flex h-screen">
+        <div className="flex-1 flex flex-col">
+          <Header />
+
+          {/* main */}
+          <main className="flex-1 overflow-y-auto bg-[#f8f8f8]">
+            Main
+          </main>
+
+          <Footer />
+        </div>
+      </div>
+      <Toaster richColors position="top-center" />
     </>
   )
 }
